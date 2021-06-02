2d4baf836abd0d246f2314e26962bb4533f9f7fb
Akhil Goyal <akhilgoyal@akhils-mbp.lan>
2021-06-02 17:39:02 -0400

    Added mock data for asynchronous testing.

--- script2.test.js
+++ script2.test.js
@@ -27,3 +27,23 @@ it('calls swapi to get people with a promise', () => {
         expect(data.results.length).toBeGreaterThan(5);
     });
 });
+
+it('getPeople returns count & results', () => {
+    const mockFetch = jest.fn()
+        .mockReturnValue(Promise.resolve({
+            json: () => Promise.resolve({
+                count: 87,
+                results: [0, 1, 2, 3, 4, 5]
+            })
+        }));
+
+    expect.assertions(4);
+
+    return swapi.getPeoplePromise(mockFetch)
+        .then(data => {
+            expect(mockFetch.mock.calls.length).toBe(1);
+            expect(mockFetch).toBeCalledWith(`http://swapi.py4e.com/api/people/`);
+            expect(data.count).toEqual(87);
+            expect(data.results.length).toBeGreaterThan(5);
+        })
+});
